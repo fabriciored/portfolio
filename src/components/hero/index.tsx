6fc2ee46ea34bc329b272bf9941fa12943784fa4
Fabrício Santos <fabriciojlsantos@gmail.com>
2024-05-07 02:28:05 -0300

    feat: hero component

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,12 @@
+import "react-multi-carousel/lib/styles.css";
+import Slider from "./slider";
+import Profile from "./profile";
+
+export default function Hero() {
+  return (
+    <div className="flex p-2 flex-col ">
+      <Profile />
+      <Slider />
+    </div>
+  );
+}
