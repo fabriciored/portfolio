41e561d8d3cb446da9b84e36465e8c3c9d560453
Fabrício Santos <fabriciojlsantos@gmail.com>
2024-05-07 02:27:38 -0300

    chore: add background image

--- src/layout/page/centered/index.tsx
+++ src/layout/page/centered/index.tsx
@@ -3,7 +3,7 @@ import { ComponentChildrenProps } from "@/intefaces/children";
 export default function CenteredPage(props: ComponentChildrenProps) {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center px-24 py-12 bg-zinc-900 bg-home bg-center bg-cover bg-fixed">
         {props.children}
       </main>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          'home': "url('/images/background/bg-home.png')",
       },
       fontFamily: { 
         "lato": ['Lato', 'sans-serif'] 
