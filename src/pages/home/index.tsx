5c7de7d4804db741b581de04a197981b22bb36eb
Fabrício Santos <fabriciojlsantos@gmail.com>
2024-05-07 02:28:31 -0300

    refactor: separate home page from app index

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/hero";
+import CenteredPage from "@/layout/page/centered";
+
+export default function Home() {
+    return (
+        <CenteredPage>
+        <Hero/>
+      </CenteredPage>
+    );
+  }
+  
