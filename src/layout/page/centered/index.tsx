bde8f8bd5850bac250429b2195dad2f2f766f09a
Fabrício Santos <fabriciojlsantos@gmail.com>
2025-01-28 13:08:00 -0300

    fix: centered page overflow

--- src/layout/page/centered/index.tsx
+++ src/layout/page/centered/index.tsx
@@ -3,7 +3,7 @@ import { ComponentChildrenProps } from "@/intefaces/children";
 export default function CenteredPage(props: ComponentChildrenProps) {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center px-24 py-12 bg-zinc-900 bg-home bg-center bg-cover bg-fixed">
+      <main className="flex min-h-screen flex-col items-center px-24 py-12 bg-zinc-900 bg-home bg-center bg-cover bg-fixed overflow-hidden">
         {props.children}
       </main>
     </>
