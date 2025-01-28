744385e75285a2ebfbf7616b4020ac3963c2d7d4
Fabrício Santos <fabriciojlsantos@gmail.com>
2025-01-28 13:08:24 -0300

    feat: portfolio page

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,10 +1,36 @@
+import ListControls from "@/components/list-controls";
+import ProjectsList from "@/components/lists/projects";
 import CenteredPage from "@/layout/page/centered";
+import { Project } from "@/types";
+import fetchData from "@/utils/fetch";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Portfolio() {
-    return (
-      <CenteredPage>
-        Portfolio
-      </CenteredPage>
-    );
-  }
-  
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+  const [maxPages, setMaxPages] = useState<number | null>(null);
+
+
+  useEffect(() => {
+    fetchData({
+      entity: projects as Project[],
+      route: "projects",
+      page,
+      limit,
+      setData: setProjects,
+      setMaxPages,
+    });
+;
+  }, [page, limit, maxPages]);
+
+  return (
+    <CenteredPage>
+      <h2 className="text-3xl font-bold mb-5">Projects</h2>
+      <ListControls maxPages={maxPages as number} setLimit={setLimit} setPage={setPage} limit={limit} page={page}  />
+     <ProjectsList projects={projects}/>
+    </CenteredPage>
+  );
+}
